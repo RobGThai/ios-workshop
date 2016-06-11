9a0161288dce2272f4a967ec583f7fb7ed0006ce
RobGThai <thisisrobg@gmail.com>
2016-06-11 16:07:18 +0700

    Add Gitignore.

---
+++ ios-workshop/Model/Todo.h
@@ -0,0 +1,13 @@
+//
+//  Todo.h
+//  ios-workshop
+//
+//  Created by Poohdish Rattanavijai on 6/11/16.
+//  Copyright © 2016 Poohdish Rattanavijai. All rights reserved.
+//
+
+#ifndef Todo_h
+#define Todo_h
+
+
+#endif /* Todo_h */
---
+++ ios-workshop/Todo.m
@@ -0,0 +1,9 @@
+//
+//  Todo.m
+//  ios-workshop
+//
+//  Created by Poohdish Rattanavijai on 6/11/16.
+//  Copyright © 2016 Poohdish Rattanavijai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
