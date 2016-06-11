7afacbe95a75558d64fbe3cdc598ffc2871058cc
RobGThai <thisisrobg@gmail.com>
2016-06-11 13:32:58 +0700

    Initial Commit

---
+++ ios-workshop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-workshop
+//
+//  Created by Poohdish Rattanavijai on 6/11/16.
+//  Copyright © 2016 Poohdish Rattanavijai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-workshop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-workshop
+//
+//  Created by Poohdish Rattanavijai on 6/11/16.
+//  Copyright © 2016 Poohdish Rattanavijai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
